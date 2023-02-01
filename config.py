882ec7c9152558c2cfa3a311e6bf29cf824317f8
anuragmirrorleech <115845369+anuragmirrorleech@users.noreply.github.com>
2023-02-01 14:53:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
     DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://classic:classic@cluster0.skzdeyi.mongodb.net/?retryWrites=true&w=majority")
     DATABASE_NAME = os.environ.get("DATABASE_NAME","Cluster0")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Forward_data')
-    SESSION = os.environ.get("SESSION", "AQAXvxJ2peuhZaHzEHreGQuEGuxA_naxzgb6-JUxCOXknjvpwfdxDlyc5ZpGThJ-EeiPN9bXbptVTybQD7E5l_lKCKAgAF2U7XHEddk2DB8TaiIK0FjwCgC2pZOhjI9SfQtJ1ArZXtIozt9XoqfogG1BS8I9DkD37NfJez9h5NbRe11MDV_yZhPicjdTNuRZFlnUhXfM2gs4OWCEmMhSA_6by0Zb-r4hIV6laEvb9ji2V-nleqKM_C6DBNS3-IX4FrfIkpASKHXzYh6BWt0HIe2XQF9nkupCKMLA4VeYgC7Wmxu6cyU1ADnVUFTZcNjkdTY17WvDrSFJs8YOWYdTgxZmAAAAAVlFNcEA")
+    SESSION = os.environ.get("SESSION")
     TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001830106391"))
     BOT_USERNAME= os.environ.get("BOT_USERNAME", "Auto_Frwd_Robot")
 
