31bc03bbaa48e887579eb4bf2c218af240528866
anuragmirrorleech <115845369+anuragmirrorleech@users.noreply.github.com>
2023-02-01 14:47:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 import logging
 class Config:
     
-    API_ID = int(os.environ.get("API_ID", 21818317))
+    API_ID = int(os.environ.get("API_ID", "21818317"))
     API_HASH = os.environ.get("API_HASH", "bc6ab154300cc41fe127ca4d658dc75d")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6129497914:AAG00QCir0bmwjIYzoHCP5_rRu3xKS9Y9mA") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "Forward_BOT") 
@@ -11,8 +11,8 @@ class Config:
     DATABASE_NAME = os.environ.get("DATABASE_NAME","Cluster0")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Forward_data')
     SESSION = os.environ.get("SESSION", "AQAXvxJ2peuhZaHzEHreGQuEGuxA_naxzgb6-JUxCOXknjvpwfdxDlyc5ZpGThJ-EeiPN9bXbptVTybQD7E5l_lKCKAgAF2U7XHEddk2DB8TaiIK0FjwCgC2pZOhjI9SfQtJ1ArZXtIozt9XoqfogG1BS8I9DkD37NfJez9h5NbRe11MDV_yZhPicjdTNuRZFlnUhXfM2gs4OWCEmMhSA_6by0Zb-r4hIV6laEvb9ji2V-nleqKM_C6DBNS3-IX4FrfIkpASKHXzYh6BWt0HIe2XQF9nkupCKMLA4VeYgC7Wmxu6cyU1ADnVUFTZcNjkdTY17WvDrSFJs8YOWYdTgxZmAAAAAVlFNcEA")
-    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", -1001830106391))
-    BOT_USERNAME= os.environ.get("BOT_USERNAME", 'Auto_Frwd_Robot')
+    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001830106391"))
+    BOT_USERNAME= os.environ.get("BOT_USERNAME", "Auto_Frwd_Robot")
 
 
 def LOGGER(name: str) -> logging.Logger:
